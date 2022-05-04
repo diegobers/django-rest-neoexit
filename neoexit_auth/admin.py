24e1b0bebd6f46ede3128420cb82ce2bd5df1a0c
Diego Bernardi <diego-bs@hotmail.com>
2022-05-04 15:28:19 -0300

    custom user model

--- neoexit_auth/admin.py
+++ neoexit_auth/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from neoexit_auth.models import User
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
