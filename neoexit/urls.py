101c85de55977fda5cbf847f3474d7ff5f1e079a
Diego Bernardi <diego-bs@hotmail.com>
2022-05-05 22:48:53 -0300

    user profile template and view

--- neoexit/urls.py
+++ neoexit/urls.py
@@ -5,5 +5,7 @@ import oferta.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', oferta.views.index),
+    path('', oferta.views.index, name='index'),
+    path('oferta/<slug>/', oferta.views.oferta_detail, name='oferta-detalhes'),
+    #path("accounts/profile/", oferta_auth.views.profile, name="profile"),
 ]
