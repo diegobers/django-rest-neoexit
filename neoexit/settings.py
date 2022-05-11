8ec6f32fc4e58fc5577611256074ce8f48c6f574
Diego Bernardi <diego-bs@hotmail.com>
2022-05-10 22:00:21 -0300

    update env var language-code and time-zone

--- neoexit/settings.py
+++ neoexit/settings.py
@@ -100,9 +100,9 @@ class Dev(Configuration):
     # Internationalization
     # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'pt-BR'
 
-    TIME_ZONE = values.Value("UTC")
+    TIME_ZONE = values.Value("America/Sao_Paulo")
 
     USE_I18N = True
 
