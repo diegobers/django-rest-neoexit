47fde19383c6d51968490ef931c653ebaab9a9c1
Diego Bernardi <diego-bs@hotmail.com>
2022-05-10 21:31:35 -0300

    add django dev configuration

--- neoexit/wsgi.py
+++ neoexit/wsgi.py
@@ -1,7 +1,9 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neoexit.settings')
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
