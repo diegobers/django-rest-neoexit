f73cb4e23e411c1045a2d78cbda0fbb43daee8bd
Diego Bernardi <diego-bs@hotmail.com>
2022-05-23 20:18:19 -0300

    update templates

--- oferta/views.py
+++ oferta/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+
 from oferta.models import Oferta
 from oferta.forms import ComentarioForm
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormMixin
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 def index(request):
     # filter(created_at__lte=timezone.now()).select_related("author").only/.defer("title")
@@ -12,24 +15,51 @@ def index(request):
     #logger.debug("%d ofertas", len(ofer))
 
     return render(request, "oferta/index.html", {"oferta": ofer})
-
-
-def oferta_detail(request, slug):
-    oferta = get_object_or_404(Oferta, slug=slug)
-
-    if request.user.is_active:
-        if request.method == "POST":
-            comentario_form = ComentarioForm(request.POST)
-            
-            if comentario_form.is_valid():
-                comentario = comentario_form.save(commit=False)
-                comentario.content_object = oferta
-                comentario.creator = request.user
-                comentario.save()
-                return redirect(request.path_info)
-        else:
-            comentario_form = ComentarioForm()
-    else:
-        comentario_form = None
     
-    return render(request, "oferta/oferta-detalhe.html", {"oferta": oferta, "comentario_form": comentario_form})
+class OfertaList(ListView):
+    model = Oferta
+    template_name = 'oferta/listar.html'
+    context_object_name = 'oferta'
+
+class OfertaCreate(CreateView):
+    model = Oferta
+    template_name = 'oferta/registrar.html'
+    fields = '__all__'
+    context_object_name = 'oferta'
+    success_url = reverse_lazy('oferta')
+
+    def form_valid(self, form):
+        form.instance.cliente = self.request.user
+        #form.instance.img = self.request.FILES
+        
+        return super(OfertaCreate, self).form_valid(form)
+
+class OfertaUpdate(UpdateView):
+    model = Oferta
+    template_name = 'oferta/registrar.html'
+    context_object_name = 'oferta'
+    fields = '__all__'
+    success_url = reverse_lazy('oferta')
+
+class OfertaDelete(DeleteView):
+    model = Oferta
+    template_name = 'ofertar/deletar.html'
+    context_object_name = 'oferta'
+    success_url = reverse_lazy('oferta')
+
+class OfertaDetail(FormMixin, DetailView):
+    model = Oferta
+    template_name = 'oferta/detalhes.html' 
+    form_class = ComentarioForm
+
+    def post(self, request, *args, **kwargs):
+        comentario_form = self.get_form()
+        self.object = self.get_object()
+        if comentario_form.is_valid():
+            comentario = comentario_form.save(commit=False)
+            comentario.content_object = self.object
+            comentario.creator = self.request.user
+            comentario.save()
+            return redirect(request.path_info)
+        else:
+            return self.form_invalid(comentario_form)
